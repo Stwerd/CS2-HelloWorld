0e27213a9f1bcda6127269af52d5effb1ba6498e
Jackson <jacko7557@gmail.com>
2015-05-06 08:55:05 -0700

    Added scanning function
    Asked for your name then ended by saying goodbye + your name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class HelloWorld
 {
+  private static Scanner in = new Scanner(System.in);
+
   public static void main(String[] args)
   {
     System.out.println("Hello World!");
-    System.out.println("NORMIES GET OUT REEEEEEEEEEEEEEE");
-    System.out.println("Goodbye");
+    System.out.println("Whats your name?!");
+    String name = in.nextLine();
+    System.out.println("Goodbye " + name +"!");
   }
 }
