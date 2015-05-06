d273805214ce079f63d002be71c65270925d65d0
Jackson <jacko7557@gmail.com>
2015-05-06 08:48:43 -0700

    added functionality for printing hello world

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+    System.out.println("NORMIES GET OUT REEEEEEEEEEEEEEE");
+    System.out.println("Goodbye");
+  }
+}
